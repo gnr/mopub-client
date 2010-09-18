bac4df8515552bfe6c36eb75c51fd8600e29889a
Bryan Atwood <batwood@mopub.com>
2010-09-18 16:04:23 -0700

    Updates Android SDK to get the redirect URL from the response header

--- Android/SDK/com/mopub/mobileads/AdWebViewClient.java
+++ Android/SDK/com/mopub/mobileads/AdWebViewClient.java
@@ -1,14 +1,18 @@
 package com.mopub.mobileads;
 
 import java.io.IOException;
+import java.net.URI;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.RedirectHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HttpContext;
+
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.provider.Settings.Secure;
 import android.util.Log;
 import android.webkit.WebView;
@@ -17,7 +21,6 @@ import android.webkit.WebViewClient;
 class AdWebViewClient extends WebViewClient {
 	private static final String BASE_ACLK_URL = "http://www.mopub.com/m/aclk";
 	private AdView adView;
-	private String mUri;
 	
 	AdWebViewClient(AdView adView) {
 		this.adView = adView;
@@ -26,14 +29,10 @@ class AdWebViewClient extends WebViewClient {
 	@Override
 	public boolean shouldOverrideUrlLoading(WebView view, String url) {
 		Log.i("url", url);
-		if (url.startsWith("http://www.mopub-inc.com")) {
-			view.loadUrl(url);
-			return true;
-		}
 
 		// Route the ad click through our backend for tracking purposes
 		StringBuilder sz = new StringBuilder(BASE_ACLK_URL);
-		sz.append("?v=1&id=" + this.adView.getPublisherId());
+		sz.append("?v=1&id=" + this.adView.getAdUnitId());
 		sz.append("&udid=" + System.getProperty(Secure.ANDROID_ID));
 		if (this.adView.getKeywords() != null) {
 			sz.append("&q=" + Uri.encode(adView.getKeywords()));
@@ -42,28 +41,45 @@ class AdWebViewClient extends WebViewClient {
 			sz.append("&ll=" + (this.adView.getLocation().getLatitudeE6() / 1000000.0) + "," + (this.adView.getLocation().getLongitudeE6() / 1000000.0));
 		}
 		sz.append("&r=" + Uri.encode(url));
-		mUri = sz.toString();
 		
 		// Log the request asynchronously
-		Thread trackThread = new Thread(mTrackRunnable);
-		trackThread.start();
-		
+		String uri = sz.toString();
+		Log.i("aclk", uri);
+		new TrackClickTask().execute(uri);
+
 		// and fire off a system wide intent
 		adView.getContext().startActivity(new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(url)));
 		return true;
 	}
 	
-    Runnable mTrackRunnable = new Runnable() {
-        public void run() {          		
-    		Log.i("aclk", mUri);
+	@Override
+	public void onPageFinished(WebView view, String url) {
+		
+	}
+	
+	 private class TrackClickTask extends AsyncTask<String, Void, Void> {
+	     protected Void doInBackground(String... urls) {
+	    	 DefaultHttpClient httpclient = new DefaultHttpClient();
+	    	 // TODO: Would be really nice if we didn't have to do this redirect weirdness
+	    	 httpclient.setRedirectHandler(new RedirectHandler() {
+	    		 public URI getLocationURI(HttpResponse response,
+	    				 HttpContext context) {
+	    			 return null;
+	    		 }
 
-        	HttpClient httpclient = new DefaultHttpClient();  
-        	HttpGet httpget = new HttpGet(mUri);  
-        	
-    		try {
-    			httpclient.execute(httpget);  
-    		} catch (IOException e) {
-    		}
-        }
-    };	
+	    		 public boolean isRedirectRequested(HttpResponse response,
+	    				 HttpContext context) {
+	    			 return false;
+	    		 }
+	    	 });
+
+	    	 HttpGet httpget = new HttpGet(urls[0]);  
+
+	    	 try {
+	    		 httpclient.execute(httpget);  
+	    	 } catch (IOException e) {
+	    	 }
+	    	 return null;
+	     }
+	 }
 }
