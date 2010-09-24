67af211e6788556e52d6294a86fd0d8520eb0157
Nafis Jamal <njamal@Macintosh.local>
2010-09-24 01:43:52 -0700

    Leaked checked and ready for testing still a few hardcoded request to force interstital

---
+++ iPhone/Classes/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  SimpleAds
+//
+//  Created by Nafis Jamal on 9/24/10.
+//  Copyright 2010 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondViewController : UIViewController {
+
+}
+
+@end
