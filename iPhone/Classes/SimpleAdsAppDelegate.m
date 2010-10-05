d14980dc56612885f11d57748bbe3157ae5088dc
James Payne <jpayne@mopub-inc.com>
2010-10-05 14:06:59 -0700

    cleanup comments in sample

--- iPhone/Classes/SimpleAdsAppDelegate.m
+++ iPhone/Classes/SimpleAdsAppDelegate.m
@@ -18,17 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     
     // Override point for customization after app launch    
-	
-	
 	viewController = [[SimpleAdsViewController alloc] initWithNibName:@"SimpleAdsViewController" bundle:nil];
 	navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
-	viewController.navigationItem.title = @"Nav Controller";
-	
-	
-//	interstitialAdController = [[InterstitialAdController alloc] initWithAdUnitId:PUB_ID_INTERSTITIAL parentViewController:nil];
-//	interstitialAdController.delegate = viewController; // the SimpleAdsViewController Object controller is response for showing the ad once its ready
-//	[interstitialAdController loadAd];
-	
+	viewController.navigationItem.title = @"MoPub Demo";
 	
     [window addSubview:navigationController.view];
     [window makeKeyAndVisible];
