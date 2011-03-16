424f615386be5fd4da2496612d4f320f11b7847b
Bryan Atwood <batwood@mopub.com>
2011-03-15 19:16:31 -0700

    Fix URL to point to live server

--- Android/mopub-android-sdk/src/com/mopub/mobileads/simpleadsdemo/SimpleAdsDemoConstants.java
+++ Android/mopub-android-sdk/src/com/mopub/mobileads/simpleadsdemo/SimpleAdsDemoConstants.java
@@ -1,8 +1,7 @@
 package com.mopub.mobileads.simpleadsdemo;
 
 public abstract class SimpleAdsDemoConstants {
-    public static final String PUB_ID_320x50 = "agltb3B1Yi1pbmNyCgsSBFNpdGUYAgw";
-    //public static final String PUB_ID_320x50 = "agltb3B1Yi1pbmNyDAsSBFNpdGUY2aQGDA";
+    public static final String PUB_ID_320x50 = "agltb3B1Yi1pbmNyDAsSBFNpdGUYkaoMDA";
     public static final String PUB_ID_300x250 = "agltb3B1Yi1pbmNyDAsSBFNpdGUYycEMDA";
     public static final String PUB_ID_INTERSTITIAL = "agltb3B1Yi1pbmNyDAsSBFNpdGUYsckMDA";
 }
