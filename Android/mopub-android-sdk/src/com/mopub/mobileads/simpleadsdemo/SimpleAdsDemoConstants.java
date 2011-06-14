95884aff16c8897d812adb317d8d4952985340b1
Gabriel Vanrenen <gabe@skimble.com>
2011-06-13 17:07:15 -0700

    removing sample mopub classes

--- Android/mopub-android-sdk/src/com/mopub/mobileads/simpleadsdemo/SimpleAdsDemoConstants.java
+++
@@ -1,7 +0,0 @@
-package com.mopub.mobileads.simpleadsdemo;
-
-public abstract class SimpleAdsDemoConstants {
-    public static final String PUB_ID_320x50 = "agltb3B1Yi1pbmNyDAsSBFNpdGUYkaoMDA";
-	public static final String PUB_ID_300x250 = "agltb3B1Yi1pbmNyDAsSBFNpdGUYycEMDA";
-    public static final String PUB_ID_INTERSTITIAL = "agltb3B1Yi1pbmNyDAsSBFNpdGUYsckMDA";
-}
