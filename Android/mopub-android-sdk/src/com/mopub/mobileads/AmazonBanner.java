ed1a1a9107f58efcee1d36f71ad33ecf36dcf4d9
John Tighe <john@skimble.com>
2013-06-09 18:47:07 +0200

    Cleaning up AmazonBanner class

--- Android/mopub-android-sdk/src/com/mopub/mobileads/AmazonBanner.java
+++ Android/mopub-android-sdk/src/com/mopub/mobileads/AmazonBanner.java
@@ -28,45 +28,39 @@ public class AmazonBanner extends CustomEventBanner implements AdListener {
          * You may also pass this String down in the serverExtras Map by specifying Custom Event Data
          * in MoPub's web interface.
          */
-//        String amazonAppId = "YOUR_AMAZON_APP_ID";
         Log.d("AmazonBanner", "loadAd()");
         mAmazonAd = new AdLayout(context, AdSize.AD_SIZE_320x50);
         mAmazonAd.setListener(this);
         mAmazonAd.loadAd(new AdTargetingOptions());
-
-//        mAmazonAd.refresh();
+		mBannerListener.setAdContentView(mAmazonAd);
     }
 
     @Override
 	public void onInvalidate() {
-//        mAmazonAd.setListener(this);
         Log.d("AmazonBanner", "onInvalidate()");
+        mAmazonAd.destroy();
+        mAmazonAd = null;
     }
 
 	@Override
 	public void onAdCollapsed(AdLayout view) {
-		// TODO Auto-generated method stub
         Log.d("AmazonBanner", "onAdCollapsed()");
 	}
 
 	@Override
 	public void onAdExpanded(AdLayout view) {
-		// TODO Auto-generated method stub
         Log.d("AmazonBanner", "onAdExpanded()");
 	}
 
 	@Override
 	public void onAdFailedToLoad(AdLayout view, AdError error) {
-		// TODO Auto-generated method stub
-//      Log.d("MoPub", "Greystripe banner ad failed to load.");
-        Log.d("AmazonBanner", "onAdFailedToLoad()");
-      mBannerListener.onAdFailed();
+		Log.d("AmazonBanner", "onAdFailedToLoad()");
+		mBannerListener.onAdFailed();
 	}
 
 	@Override
 	public void onAdLoaded(AdLayout view, AdProperties adProperties) {
         Log.d("AmazonBanner", "onAdLoaded()");
 		mBannerListener.onAdLoaded();
-		mBannerListener.setAdContentView(mAmazonAd);
 	}
 }
